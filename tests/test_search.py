8844622462821c4a5e394b8f46016609f1e90694
Egor Mahlaev <mahlasza@gmail.com>
2019-06-30 20:55:22 +0300

    Add some changes to SearchTest.

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,41 +1,35 @@
 from algorithms.search import (
-    binary_search_iterative,
-    binary_search_recursive,
+    binary_search_iterative as bsi,
+    binary_search_recursive as bsr,
     linear_search
 )
 
 import unittest
 
 
-class TestSearch(unittest.TestCase):
+class SearchTest(unittest.TestCase):
 
     def setUp(self):
-        self.array = [1, 2, 3, 4, 5]
+        self.size = 5 # test array size
+        self.array = [i for i in range(self.size)] # test array
 
     def test_linear_search(self):
-        self.assertEqual(linear_search(self.array, 0), -1)
-        self.assertEqual(linear_search(self.array, 1), 0)
-        self.assertEqual(linear_search(self.array, 2), 1)
-        self.assertEqual(linear_search(self.array, 3), 2)
-        self.assertEqual(linear_search(self.array, 4), 3)
-        self.assertEqual(linear_search(self.array, 5), 4)
-        self.assertEqual(linear_search(self.array, 6), -1)
-
+        for i in range(self.size):
+            with self.subTest(i=i):
+                self.assertEqual(linear_search(self.array, i), i)
+        self.assertEqual(linear_search(self.array, -1), -1)
+        self.assertEqual(linear_search(self.array, self.size), -1)
+        
     def test_binary_search_iterative(self):
-        self.assertEqual(binary_search_iterative(self.array, 0), -1)
-        self.assertEqual(binary_search_iterative(self.array, 1), 0)
-        self.assertEqual(binary_search_iterative(self.array, 2), 1)
-        self.assertEqual(binary_search_iterative(self.array, 3), 2)
-        self.assertEqual(binary_search_iterative(self.array, 4), 3)
-        self.assertEqual(binary_search_iterative(self.array, 5), 4)
-        self.assertEqual(binary_search_iterative(self.array, 6), -1)
+        for i in range(self.size):
+            with self.subTest(i=i):
+                self.assertEqual(bsi(self.array, i), i)
+        self.assertEqual(bsi(self.array, -1), -1)
+        self.assertEqual(bsi(self.array, self.size), -1)
 
     def test_binary_search_recursive(self):
-        limit = len(self.array) - 1 # initial value for "right" argument
-        self.assertEqual(binary_search_recursive(self.array, 0, 0, limit), -1)
-        self.assertEqual(binary_search_recursive(self.array, 1, 0, limit), 0)
-        self.assertEqual(binary_search_recursive(self.array, 2, 0, limit), 1)
-        self.assertEqual(binary_search_recursive(self.array, 3, 0, limit), 2)
-        self.assertEqual(binary_search_recursive(self.array, 4, 0, limit), 3)
-        self.assertEqual(binary_search_recursive(self.array, 5, 0, limit), 4)
-        self.assertEqual(binary_search_recursive(self.array, 6, 0, limit), -1)
+        for i in range(self.size):
+            with self.subTest(i=i):
+                self.assertEqual(bsr(self.array, i, 0, self.size - 1), i)
+        self.assertEqual(bsr(self.array, -1, 0, self.size - 1), -1)
+        self.assertEqual(bsr(self.array, self.size, 0, self.size - 1), -1)
