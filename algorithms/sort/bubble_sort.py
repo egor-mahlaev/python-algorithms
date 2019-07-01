24d66debea7ae5f87bfc140d97849cd74492a576
Egor Mahlaev <mahlasza@gmail.com>
2019-07-01 21:12:56 +0300

    Add Bubble Sort Algorithm.

---
+++ algorithms/sort/bubble_sort.py
@@ -0,0 +1,23 @@
+# Stable: Yes
+# Requires additional memory: No
+# Number of exchanges: O(n^2)
+# Best case: O(n)
+# Worst case: O(n^2)
+
+def bubble_sort(array):
+    def swap(i, j):
+        tmp = array[i]
+        array[i] = array[j]
+        array[j] = tmp
+
+    n = len(array)
+    i = 0
+    f = True
+
+    while f:
+        f = False
+        for j in range(n - i - 1):
+            if array[j] > array[j+1]:
+                swap(j, j+1)
+                f = True 
+        i += 1
